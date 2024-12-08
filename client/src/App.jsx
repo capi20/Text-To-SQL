8f95c0729a6ee577c23e5986d8bb13eab6ff25ac
shyam <shyam20998@gmail.com>
2024-12-08 17:50:51 +0530

    added routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,27 @@
-import Dashboard from "./pages/Dashboard";
+import { Navigate, Route, Routes } from "react-router-dom";
+import { SchemaVisualizer } from "./pages/SchemaVisualizer";
+import AppLayout from "./components/AppLayout";
+import { AppProvider } from "./context/appContext";
+import { CLIENT_ROUTES } from "./constants";
+import Home from "./pages/Home";
+import ChatScreen from "./pages/ChatScreen";
 
 function App() {
-	return <Dashboard />;
+	return (
+		<AppProvider>
+			<Routes>
+				<Route path={CLIENT_ROUTES.home.path} element={<AppLayout />}>
+					<Route path="/" element={<Home />} />
+					<Route path={`chat/:id`} element={<ChatScreen />} />
+					<Route
+						path={CLIENT_ROUTES.schemaVisualizer.path}
+						element={<SchemaVisualizer />}
+					/>
+				</Route>
+				<Route path="*" element={<Navigate to="/" />} />
+			</Routes>
+		</AppProvider>
+	);
 }
 
 export default App;
